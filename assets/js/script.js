5882790c39ee03ae8490ee296f63bb055aba0f41
paulynson <paulabakada@gmail.com>
2021-03-29 17:20:14 +0100

    hello

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+
+window.addEventListener("scroll", ()=>{
+    const tour = document.querySelector(".tour");
+    tour.classList.toggle("active", scrollY >= 40);
+})
+
+
+
+const div = document.querySelector(".div");
+const inp = document.querySelector(".inside");
+const exa = document.querySelector(".exa");
+
+function myKey(){
+// inp.addEventListener("keydown", ()=>{
+if(inp.value !== " "){
+    alert("Form Submitted");
+
+} else {
+    alert("Input cannot be empty");
+
+}
+  
+}
